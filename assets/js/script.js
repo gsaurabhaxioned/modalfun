3988a0352de186e2f1b325f1633fe27be6780bb9
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-21 21:03:53 +0530

    js completed for modal box

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,30 @@ $(document).ready(function(){
          $(".menus").slideToggle();
         $(this).toggleClass("cross");
     });
+
 });
 
-let buttons = document.querySelectorAll('.portfolio-buttons');
-let tabs = document.querySelectorAll('')
+let modal_image = document.querySelectorAll('.modal-click'),
+modal_body = document.querySelector('.modal-body'),
+modal_box = document.querySelector('.modal-box'),
+image_source = document.querySelector('.modal-image');
+
+modal_image.forEach((i,index)=>{
+    i.addEventListener('click',()=>{
+        modal_body.style.display = "block";
+        modal_box.style.display = "block";
+         image_source.src = modal_image[index].src;
+    console.log(modal_image[index].src);
+    });
+})
+
+modal_body.addEventListener('click',function(){
+    modal_body.style.display = "none";
+    modal_box.style.display = "none";
+})
+
+// let buttons = document.querySelectorAll('.portfolio-buttons');
+// let tabs = document.querySelectorAll('')
 
 
 
