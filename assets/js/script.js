7ae7ad18eb8840ce984060ca545fc539994bae78
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-21 18:51:07 +0530

    javascript added for hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
-/* Author: 
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+         $(".menus").slideToggle();
+        $(this).toggleClass("cross");
+    });
+});
 
-*/
+let buttons = document.querySelectorAll('.portfolio-buttons');
+let tabs = document.querySelectorAll('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
