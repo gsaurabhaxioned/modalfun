0e10952a08772444ffc92a04b4baf7324d4a2310
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-22 09:06:17 +0530

    js for tab icons in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,17 @@ $(document).ready(function(){
 let modal_image = document.querySelectorAll('.modal-click'),
 modal_body = document.querySelector('.modal-body'),
 modal_box = document.querySelector('.modal-box'),
-image_source = document.querySelector('.modal-image');
-
+image_source = document.querySelector('.modal-image'),
+buttons = document.querySelectorAll('.portfolio-buttons'),
+tabs = document.querySelectorAll('.tab-icons'),
+main_icons = document.querySelector('.portfolio-icons');
+console.log(tabs);
 modal_image.forEach((i,index)=>{
     i.addEventListener('click',()=>{
         modal_body.style.display = "block";
         modal_box.style.display = "block";
          image_source.src = modal_image[index].src;
-    console.log(modal_image[index].src);
+    console.log(modal_image[index].src)
     });
 })
 
@@ -24,6 +27,20 @@ modal_body.addEventListener('click',function(){
     modal_body.style.display = "none";
     modal_box.style.display = "none";
 })
+
+buttons.forEach((i,index)=>{
+    i.addEventListener('click',function(){
+        if(index == 0){
+            main_icons.style.display = "block";
+        }else {
+            main_icons.style.display = "none";
+        }
+        tabs.forEach(j => {
+            j.style.left = "-150%";
+        })
+        tabs[index - 1].style.left="0";
+    })
+});
 
 // let buttons = document.querySelectorAll('.portfolio-buttons');
 // let tabs = document.querySelectorAll('')
